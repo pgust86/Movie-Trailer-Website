ceaabe8a00548a71070c0c725c1e98909c91a5f6
pgust86 <38011904+pgust86@users.noreply.github.com>
2018-04-02 17:39:26 -0400

    Changes made
    changes made in formatting

--- movies/media.py
+++ movies/media.py
@@ -1,14 +1,17 @@
 import webbrowser
 
+
 class Movie():
-    """This class provides a way to store movie related information"""
-    #initializes object with variables,movie_title, poster_image and trailer_youtube
+    """This class provides a way to store movie related information
+
+    The class takes in a movie title, poster image and youtube trailer and
+    uses the information to add data to a website"""
+    # initializes obj with var,movie_title, poster_image and trailer_youtube
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    #opens trailer in youtube for the object
+    # opens trailer in youtube for the object
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-    
